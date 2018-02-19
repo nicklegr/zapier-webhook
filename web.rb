0e14b6d5b3e320a9194827ecb18a68a499653a19
nicklegr <addr.for+github@gmail.com>
2018-02-19 23:31:29 +0900

    たぶんこれで画像のURLを取得できる

--- web.rb
+++ web.rb
@@ -4,8 +4,30 @@ require "bundler"
 Bundler.require
 
 require "json"
+require_relative "services"
 
 post "/images_from_card" do
   json = JSON.parse(request.body.read)
-  json.to_json
+
+  name = json["card_name"]
+  desc = json["card_desc"]
+
+  status_url_regexp = %r|https?://twitter.com/\w+/status(?:es)?/\d+|
+
+  status_urls = name.scan(status_url_regexp) + desc.scan(status_url_regexp)
+  status_urls.compact!
+  status_urls.uniq!
+
+  # どのTwitterクライアントでシェアしても、一番最後にそのツイートのURLが入る
+  # ツイート本文に別のツイートのURLがあっても無視する
+  status_url = status_urls.last
+
+  result =
+    if PicTwitter.support?(status_url)
+      PicTwitter.image_urls(status_url)
+    else
+      []
+    end
+
+  result.to_json
 end
